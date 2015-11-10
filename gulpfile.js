63eb9ca15f8b66948439e0f675905c850d9d195c
Jonathan Duarte <jonathan.duarte@synergy-gb.com>
2015-11-10 16:40:42 -0430

    se añadio la documentacion correspondiente

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 'use strict';
 
+var gulp = require('gulp');
 var megazord = require ('megazord-sdk');
-megazord.registerScreenTasks(); 
+megazord.registerScreenTasks();
+gulp.tasks=megazord.gulp.tasks; 
